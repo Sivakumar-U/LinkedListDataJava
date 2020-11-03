c2c48e45f0ba69c7fdce76d2aa8e3fe0f934eaa0
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-03 21:23:51 +0530

    Added: Test case added elements at the top of linked list

--- src/test/java/com/blz/linkedlist/TestLinkedList.java
+++ src/test/java/com/blz/linkedlist/TestLinkedList.java
@@ -15,4 +15,18 @@ public class TestLinkedList {
 		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+
+	@Test
+	public void given3NumbersWhenAddedToLinkedListShouldBeAddedAtTop() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		LinkedList lst = new LinkedList();
+		lst.add(myFirstNode);
+		lst.add(mySecondNode);
+		lst.add(myThirdNode);
+		boolean result = LinkedList.head.equals(myThirdNode) && LinkedList.head.getNext().equals(mySecondNode)
+				&& LinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
 }
